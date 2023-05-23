628aab6742b0b818734a58743ab35020abd91dd4
RyanKaleliGabriel <ryang9241@gmail.com>
2023-05-23 17:55:36 +0300

    Front end

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const multer = require("multer");
-require('dotenv').config();
+require("dotenv").config();
 
 //All External models that have image uploads
 const Brand = require("./filemodels/brands");
@@ -12,8 +12,8 @@ const Car = require("./filemodels/cars");
 
 ///configuring our middleware
 const app = express();
-app.use(bodyParser.urlencoded({urlencoded:true}));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ urlencoded: true }));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Configuring Database Connection
@@ -26,110 +26,111 @@ app.use(express.static("public"));
 
 //Creating collection schemas
 const userSchema = new mongoose.Schema({
-    fullNames:{
-        type:String,
-        required:true
-    },
-    userName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    },
-    password:{
-        type:String,
-        minLength:8,
-        maxLength:15,
-    },
-    Issues:[{type: mongoose.Schema.Types.ObjectId, ref:'Issue'}],
-    Comments:[{type: mongoose.Schema.Types.ObjectId, ref:'Comment'}],
-    UserWishList:{type: mongoose.Schema.Types.ObjectId, ref:'WishList'},
+  fullNames: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    maxLength: 15,
+  },
+  Issues: [{ type: mongoose.Schema.Types.ObjectId, ref: "Issue" }],
+  Comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  UserWishList: { type: mongoose.Schema.Types.ObjectId, ref: "WishList" },
 });
 //Model for Users Schema
 const User = new mongoose.model("User", userSchema);
 
 //Issues schema
 const issueSchema = new mongoose.Schema({
-    issue:{
-        type:String,
-        required:true
-    },
-    author:{type: mongoose.Schema.Types.ObjectId, ref:'User'}
+  issue: {
+    type: String,
+    required: true,
+  },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 //Issues Model
 const Issue = new mongoose.model("Issue", issueSchema);
 
 //Comments schema
 const commentSchema = new mongoose.Schema({
-    comment:{
-        type:String,
-        required:true
-    },
-    author:{type: mongoose.Schema.Types.ObjectId, ref:'User'}
+  comment: {
+    type: String,
+    required: true,
+  },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 //Comments Model
 const Comment = new mongoose.model("Comment", commentSchema);
 
 //WishList Schema
 const wishlistSchema = new mongoose.Schema({
-    carsSaved:[{type: mongoose.Schema.Types.ObjectId, ref:'User'}]
+  carsSaved: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 //Wishlist Model
 const WishList = new mongoose.model("WishList", wishlistSchema);
 
-
 //Home Route
-app.get("/", function(req, res){
-    console.log("This Page should be showing");
-    res.render("home")
+app.get("/", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("home");
 });
 
 //About Page Route
-app.get("/about", function(req,res){
-    console.log("This Page should be showing");
-    res.render("about");
+app.get("/about", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("about");
 });
 
 //Edit Route
-app.get("/edit", function(req, res){
-    console.log("This Page should be showing");
-    res.render("edit");
+app.get("/edit", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("edit");
 });
 
 //One Car Page
-app.get("/car", function(req,res){
-    console.log("This Page should be showing");
-    res.render("car");
+app.get("/car", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("car");
 });
 
 //Register Route
-app.get("/register", function(req,res){
-    console.log("This Page should be showing");
-    res.render("register");
+app.get("/register", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("register");
 });
 
 //Login Route
-app.get("/login", function(req,res){
-    console.log("This Page should be showing");
-    res.render("login");
+app.get("/login", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("login");
 });
 
 //Admins Page
-app.get("/admin", function(req,res){
-    console.log("This Page should be showing");
-    res.render("admin");
+app.get("/admin", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("admin");
 });
 
 //Post Car(Admin) Route
-app.get("/posts", function(req,res){
-    console.log("This Page should be showing");
-    res.render("post");
+app.get("/posts", function (req, res) {
+  console.log("This Page should be showing");
+  res.render("post");
 });
 
-
-app.listen(process.env.PORT, function(){
-    console.log("Server is running on port 3000");
-})
+app.listen(process.env.PORT, function () {
+  console.log("Server is running on port 3000");
+});
