d24531328c4f8054c985985ea4121e82558b3396
RyanKaleliGabriel <ryang9241@gmail.com>
2023-06-13 12:25:13 +0300

    Authentication complete

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ app.post("/register", function(req,res){
       res.redirect("/register");
     }else{
       passport.authenticate("local")(req,res, function(){
-        res.redirect("/cars")
+        res.redirect("/login")
       });
     }
   });
